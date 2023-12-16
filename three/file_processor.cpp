aeb4505dc238605ea2726d191739254b18c8bfc0
Max Latoche <max.latoche@gmail.com>
2023-12-16 18:00:15 -0300

    day 3

---
+++ three/file_processor.cpp
@@ -0,0 +1,10 @@
+#include "file_processor.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <numeric>
+#include <tuple>
+
+int FileProcessor::numberFinder(const std::string &filePath)
+{
+}
---
+++ three/file_processor.h
@@ -0,0 +1,11 @@
+#ifndef FILE_PROCESSOR_H
+#define FILE_PROCESSOR_H
+
+#include <string>
+
+class FileProcessor {
+public:
+    static int numberFinder(const std::string &filePath);
+};
+
+#endif
