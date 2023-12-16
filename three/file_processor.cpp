69c70006e57f066f257484890426ea389953b8ba
Max Latoche <max.latoche@gmail.com>
2023-12-16 18:00:49 -0300

    build basic 2D array

--- three/file_processor.cpp
+++ three/file_processor.cpp
@@ -7,4 +7,30 @@
 
 int FileProcessor::numberFinder(const std::string &filePath)
 {
+    std::ifstream file(filePath);
+
+    if (!file.is_open())
+    {
+        std::cerr << "couldn't open file: " << filePath << std::endl;
+        return -1;
+    }
+
+    int runningTotal = 0;
+    std::vector<std::vector<char>> linesArr;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::vector<char> charRow;
+        for (char c : line) {
+            charRow.push_back(c);
+        }
+
+        linesArr.push_back(charRow);
+    }
+
+
+
+    file.close();
+
+    return runningTotal;
 }
